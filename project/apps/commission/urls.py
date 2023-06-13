55073738bd024cf6bf38af67a4a0b0b377cdf476
Agustin Boneu <agusboneu@hotmail.com>
2023-06-13 00:56:38 -0300

    update

--- project/apps/commission/urls.py
+++ project/apps/commission/urls.py
@@ -4,18 +4,17 @@ from . import views
 
 urlpatterns = [
     path("index commission/", views.indexcommission, name="index commission"),
-    path("create commission/", views.create_commission, name="create commission"),
-    path("input commission/", views.create_input, name="input commission"),
-    path("discount commission/", views.create_discount, name="discount commission"),
-    path("list commission/", views.commission_list, name="list commission"),
-    path("list input/", views.input_list, name="list input"),
-    path("list discount/", views.discount_list, name="list discount"),
-    path("deletecommission_list/<int:id>", views.delete_commission, name="deletecommission_list"),
-    path("deletediscount_list/<int:id>", views.delete_discount, name="deletediscount_list"),
-    path("deleteinput_list/<int:id>", views.delete_input, name="deleteinput_list"),
-    path("updatecommission_list/<int:id>", views.update_commission, name="updatecommission_list"),
-    path("updateinput_list/<int:id>", views.update_input, name="updateinput_list"),
-    path("updatediscount_list/<int:id>", views.update_discount, name="updatediscount_list"),
+    path("create commission/", views.CommissionCreate.as_view(), name="create commission"),
+    path("input commission/", views.InputCreate.as_view(), name="input commission"),
+    path("discount commission/", views.DiscountCreate.as_view(), name="discount commission"),
+    path("list commission/", views.CommissionList.as_view(), name="list commission"),
+    path("list input/", views.InputList.as_view(), name="list input"),
+    path("list discount/", views.DiscountList.as_view(), name="list discount"),
+    path("deletecommission_list/<int:pk>", views.DeleteComission.as_view() , name="deletecommission_list"),
+    path("deletediscount_list/<int:pk>", views.DeleteDiscount.as_view(), name="deletediscount_list"),
+    path("deleteinput_list/<int:pk>", views.DeleteInput.as_view(), name="deleteinput_list"),
+    path("updatecommission_list/<int:pk>", views.CommissionUpdate.as_view(), name="updatecommission_list"),
+    path("updateinput_list/<int:pk>", views.InputUpdate.as_view(), name="updateinput_list"),
+    path("updatediscount_list/<int:pk>", views.DiscountUpdate.as_view(), name="updatediscount_list"),
 ]
 
-    
--- project/apps/commission/views.py
+++ project/apps/commission/views.py
@@ -1,110 +1,193 @@
-from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from .models import Commissions_USF,Input_Commissions_USF,Discount_CommissionsBank_USF
 from . import forms
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 # Create your views here.
 def indexcommission (request):
     return render(request, 'commission/index_commission.html')  
 
-def create_commission(request,): 
-    from .forms import CommissionForm
-    if request.method == "POST":
-        form = CommissionForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse("commission:index commission"))
-    else:  
-        form = CommissionForm()
-    return render(request, "commission/create_commission.html",{"form": form})
+class CommissionCreate(CreateView):
+    model = Commissions_USF
+    forms_class = forms.CommissionForm
+    fields = "__all__"
+    template_name = "commission/create_commission.html"
+    success_url = reverse_lazy("commission:index commission")
 
-def create_input(request): 
-    form = forms.InputForm(request.POST) 
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        return redirect("commission:index commission")
-    context = {"form": form}
-    return render (request,"commission/create_input.html",context)
+# def create_commission(request,): 
+#     from .forms import CommissionForm
+#     if request.method == "POST":
+#         form = CommissionForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse("commission:index commission"))
+#     else:  
+#         form = CommissionForm()
+#     return render(request, "commission/create_commission.html",{"form": form})
 
-def create_discount(request):  
-    form = forms.DiscountForm(request.POST)
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        return redirect("commission:index commission")
-    context = {"form": form}
-    return render (request,"commission/create_discount.html",context)
+class InputCreate(CreateView):
+    model = Input_Commissions_USF
+    forms_class = forms.InputForm
+    fields = "__all__"
+    template_name = "commission/create_input.html"
+    success_url = reverse_lazy("commission:index commission")
+
+# def create_input(request): 
+#     form = forms.InputForm(request.POST) 
+#     if request.method == "POST" and form.is_valid():
+#         form.save()
+#         return redirect("commission:index commission")
+#     context = {"form": form}
+#     return render (request,"commission/create_input.html",context)
+
+class DiscountCreate(CreateView):
+    model = Discount_CommissionsBank_USF
+    forms_class = forms.DiscountForm
+    fields = "__all__"
+    template_name = "commission/create_discount.html"
+    success_url = reverse_lazy("commission:index commission")
+
+# def create_discount(request):  
+#     form = forms.DiscountForm(request.POST)
+#     if request.method == "POST" and form.is_valid():
+#         form.save()
+#         return redirect("commission:index commission")
+#     context = {"form": form}
+#     return render (request,"commission/create_discount.html",context)
+
+class CommissionList(ListView):
+    model = Commissions_USF
+    template_name = "commission/commission_list.html"
+    context_object_name = "categorias"
 
 
-def commission_list(request):
-    categorias = Commissions_USF.objects.all()
-    context = {"categorias": categorias}
-    return render (request,"commission/commission_list.html",context)
+# def commission_list(request):
+#     categorias = Commissions_USF.objects.all()
+#     context = {"categorias": categorias}
+#     return render (request,"commission/commission_list.html",context)
 
-def discount_list(request):
-    categorias = Discount_CommissionsBank_USF.objects.all()
-    context = {"categorias": categorias}
-    return render (request,"commission/discount_list.html",context)
+class DiscountList(ListView):
+    model = Discount_CommissionsBank_USF
+    template_name = "commission/discount_list.html"
+    context_object_name = "categorias"
 
-def input_list(request):
-    categorias = Input_Commissions_USF.objects.all()
-    context = {"categorias": categorias}
-    return render (request,"commission/input_list.html",context)
 
-def delete_commission (request,id):
-    categoria = Commissions_USF.objects.get(id=id)
-    if request.method == "POST":
-        categoria.delete()
-        return redirect("commission:index commission")
-    return render(request, "commission/deletecommission_list.html", {"categoria": categoria})
+# def discount_list(request):
+#     categorias = Discount_CommissionsBank_USF.objects.all()
+#     context = {"categorias": categorias}
+#     return render (request,"commission/discount_list.html",context)
 
-def delete_input (request,id):
-    categoria = Input_Commissions_USF.objects.get(id=id)
-    if request.method == "POST":
-        categoria.delete()
-        return redirect("commission:index commission")
-    return render(request, "commission/deleteinput_list.html", {"categoria": categoria})
+class InputList(ListView):
+    model = Input_Commissions_USF
+    template_name = "commission/input_list.html"
+    context_object_name = "categorias"
 
-def delete_discount (request,id):
-    categoria = Discount_CommissionsBank_USF.objects.get(id=id)
-    if request.method == "POST":
-        categoria.delete()
-        return redirect("commission:index commission")
-    return render(request, "commission/deletediscount_list.html", {"categoria": categoria})
+# def input_list(request):
+#     categorias = Input_Commissions_USF.objects.all()
+#     context = {"categorias": categorias}
+#     return render (request,"commission/input_list.html",context)
 
-def update_commission(request, id):
-    categoria = Commissions_USF.objects.get(id=id)
-    if request.method == "POST":
-        form = forms.CommissionForm(request.POST, instance=categoria)
-        if form.is_valid():
-            form.save()
-            return redirect("commission:index commission")
-    else:
-        form = forms.CommissionForm(instance=categoria)
-    return render(request, "commission/updatecommission_list.html", {"form": form})
+class DeleteComission(DeleteView):
+    model = Commissions_USF
+    forms_class = forms.CommissionForm
+    fields = "__all__"
+    template_name = "commission/deletecommission_list.html"
+    success_url = reverse_lazy("commission:index commission")
 
-def update_input(request, id):
-    categoria = Input_Commissions_USF.objects.get(id=id)
-    if request.method == "POST":
-        form = forms.InputForm(request.POST, instance=categoria)
-        if form.is_valid():
-            form.save()
-            return redirect("commission:index commission")
-    else:
-        form = forms.InputForm(instance=categoria)
-    return render(request, "commission/updateinput_list.html",{"form": form})
+
+# def delete_commission (request,id):
+#     categoria = Commissions_USF.objects.get(id=id)
+#     if request.method == "POST":
+#         categoria.delete()
+#         return redirect("commission:index commission")
+#     return render(request, "commission/deletecommission_list.html", {"categoria": categoria})
+
+class DeleteInput(DeleteView):
+    model = Input_Commissions_USF
+    forms_class = forms.InputForm
+    fields = "__all__"
+    template_name = "commission/deleteinput_list.html"
+    success_url = reverse_lazy("commission:index commission")
+
+# def delete_input (request,id):
+#     categoria = Input_Commissions_USF.objects.get(id=id)
+#     if request.method == "POST":
+#         categoria.delete()
+#         return redirect("commission:index commission")
+#     return render(request, "commission/deleteinput_list.html", {"categoria": categoria})
+
+class DeleteDiscount(DeleteView):
+    model = Discount_CommissionsBank_USF
+    forms_class = forms.DiscountForm
+    fields = "__all__"
+    template_name = "commission/deletediscount_list.html"
+    success_url = reverse_lazy("commission:index commission")
+
+# def delete_discount (request,id):
+#     categoria = Discount_CommissionsBank_USF.objects.get(id=id)
+#     if request.method == "POST":
+#         categoria.delete()
+#         return redirect("commission:index commission")
+#     return render(request, "commission/deletediscount_list.html", {"categoria": categoria})
+
+class CommissionUpdate(UpdateView):
+    model = Commissions_USF
+    forms_class = forms.CommissionForm
+    fields = "__all__"
+    template_name = "commission/updatecommission_list.html"
+    success_url = reverse_lazy("commission:index commission")
+
+
+# def update_commission(request, id):
+#     categoria = Commissions_USF.objects.get(id=id)
+#     if request.method == "POST":
+#         form = forms.CommissionForm(request.POST, instance=categoria)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("commission:index commission")
+#     else:
+#         form = forms.CommissionForm(instance=categoria)
+#     return render(request, "commission/updatecommission_list.html", {"form": form})
+
+class InputUpdate(UpdateView):
+    model = Input_Commissions_USF
+    forms_class = forms.InputForm
+    fields = "__all__"
+    template_name = "commission/updateinput_list.html"
+    success_url = reverse_lazy("commission:index commission")
+
+# def update_input(request, id):
+#     categoria = Input_Commissions_USF.objects.get(id=id)
+#     if request.method == "POST":
+#         form = forms.InputForm(request.POST, instance=categoria)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("commission:index commission")
+#     else:
+#         form = forms.InputForm(instance=categoria)
+#     return render(request, "commission/updateinput_list.html",{"form": form})
+
+class DiscountUpdate(UpdateView):
+    model = Discount_CommissionsBank_USF
+    forms_class = forms.DiscountForm
+    fields = "__all__"
+    template_name = "commission/updatediscount_list.html"
+    success_url = reverse_lazy("commission:index commission")
         
-def update_discount(request, id):
-    categoria = Discount_CommissionsBank_USF.objects.get(id=id)
-    if request.method == "POST":
-        form = forms.DiscountForm(request.POST, instance=categoria)
-        if form.is_valid():
-            form.save()
-            return redirect("commission:index commission")
-    else:
-        form = forms.DiscountForm(instance=categoria)
-    return render(request, "commission/updatediscount_list.html", {"form": form})
+# def update_discount(request, id):
+#     categoria = Discount_CommissionsBank_USF.objects.get(id=id)
+#     if request.method == "POST":
+#         form = forms.DiscountForm(request.POST, instance=categoria)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("commission:index commission")
+#     else:
+#         form = forms.DiscountForm(instance=categoria)
+#     return render(request, "commission/updatediscount_list.html", {"form": form})
 
 
 
