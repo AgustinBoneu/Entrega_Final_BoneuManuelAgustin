c984cc0d0c7fdb608550a8b90b0747bfdc82233d
Agustin Boneu <agusboneu@hotmail.com>
2023-06-14 01:45:43 -0300

    update

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.urls import path
@@ -5,7 +6,8 @@ from . import views
 
 urlpatterns = [
     path("", views.indexhome, name="index home"),
-    path("about", views.abouthome, name="about"),
+    path("about/", views.abouthome, name="about"),
+    path("login/", views.login_request, name="login"),
 ]
 urlpatterns += staticfiles_urlpatterns()
 
--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import  render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpRequest, HttpResponse
 
 
 # Create your views here.
@@ -6,4 +9,21 @@ def indexhome (request):
     return render(request, 'home/index_home.html')  
 def abouthome (request):
     return render(request, 'home/about.html')  
+
+
+
+def login_request(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+            user = authenticate(username=usuario, password=contraseña)
+            if user is not None:
+                login(request, user)
+                return render(request, 'home/index_home.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'home/login.html', {"form": form})
+
 
