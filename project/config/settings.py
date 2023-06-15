cf22850dbfba1c4f3de70447d02a06de7708ddb5
Agustin Boneu <agusboneu@hotmail.com>
2023-06-15 19:11:19 -0300

    update

--- project/config/settings.py
+++ project/config/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -130,4 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #! Login
 
 LOGIN_URL = "home:login"
+
+
 
